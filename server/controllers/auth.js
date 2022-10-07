608018910e3d54d4fa293d00ed80079076bf681c
Jessica Swenson <jessicamswens@gmail.com>
2022-10-07 17:03:44 -0600

    started building server

---
+++ server/controllers/auth.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    login: (req, res) => {
+        console.log('login function');
+    },
+    logout: (req, res) => {
+        console.log('logout function');
+    },
+}
---
+++ server/controllers/posts.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+    getAllPosts: (req, res) => {
+        console.log('getAllPosts function')
+    },
+    getCurrentPosts: (req, res) => {
+        console.log('getCurrentPosts function')
+    },
+    addPost: (req, res) => {
+        console.log('addPost function')
+    },
+    editPost: (req, res) => {
+        console.log('editPost function')
+    },
+    deletePost: (req, res) => {
+        console.log('deletePost function')
+    },
+}
